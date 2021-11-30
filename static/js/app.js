d3380aea2fac9c8fdd81ab5da2078ba2e5f89c61
Adam Schwartz <adamschwartz@Adams-MacBook-Air.local>
2021-11-30 14:53:32 -0600

    updated app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -22,3 +22,25 @@ function buildTable(data){
         );
     });
 }
+
+// create function to handle filtering
+function handleClick(){
+    // grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if a date was entered and filter the data using that date.
+    if (date){
+        // apply 'filter' to the table data to only keep the
+        // rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // rebuild the table using the filtered data
+    // if no date was entered, then filterData will be orginal table
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
