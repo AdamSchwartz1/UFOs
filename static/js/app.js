3bc9d1137fdb78ca0a83dcc0605b508430240c57
Adam Schwartz <adamschwartz@Adams-MacBook-Air.local>
2021-11-30 13:33:08 -0600

    update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,21 @@ const tableData = data;
 //reference the html table us D3
 var tbody = d3.select("tbody");
 
+function buildTable(data){
+    //first, clear out old data
+    tbody.html("")
+    // add for each loop with arrow function to loop through each object
+    // in the data and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+        // append a row to the table body
+        let row = tbody.append("tr");
+
+        // loop through each field in the dataRow
+        // and add each value as a table cell (td)
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
+}
